61be74199c1b66a404e4dda667f9ea8372e8438f
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2021-10-19 19:11:13 -0400

    Initial commit

---
+++ student.hpp
@@ -0,0 +1,16 @@
+#pragma once // prevents multiple definitions
+
+#include <iostream> // print library
+#include <string>
+using namespace std;
+
+// ===============
+// implementations
+
+//Replace string function
+int function()
+{
+	return "0"; // TODO: fix
+}
+
+
